6f177b9e72118b872b7110aa4d37bf6e7655133e
nidaeman1122 <nidaeman0987@gmail.com>
2024-12-01 20:59:47 +0500

    Initial Commoit

---
+++ lexicalAnalyzerV2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace lexicalAnalyzerV2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox tfInput;
+        private System.Windows.Forms.TextBox tfTokens;
+        private System.Windows.Forms.Button btn_Input;
+        private System.Windows.Forms.TextBox symbolTable;
+
+        private void InitializeComponent()
+        {
+            this.tfInput = new System.Windows.Forms.TextBox();
+            this.tfTokens = new System.Windows.Forms.TextBox();
+            this.btn_Input = new System.Windows.Forms.Button();
+            this.symbolTable = new System.Windows.Forms.TextBox();
+
+            this.SuspendLayout();
+
+            // tfInput
+            this.tfInput.Location = new System.Drawing.Point(12, 12);
+            this.tfInput.Multiline = true;
+            this.tfInput.Name = "tfInput";
+            this.tfInput.Size = new System.Drawing.Size(300, 100);
+            this.tfInput.TabIndex = 0;
+
+            // tfTokens
+            this.tfTokens.Location = new System.Drawing.Point(12, 150);
+            this.tfTokens.Multiline = true;
+            this.tfTokens.Name = "tfTokens";
+            this.tfTokens.ReadOnly = true;
+            this.tfTokens.Size = new System.Drawing.Size(300, 100);
+            this.tfTokens.TabIndex = 1;
+
+            // symbolTable
+            this.symbolTable.Location = new System.Drawing.Point(320, 12);
+            this.symbolTable.Multiline = true;
+            this.symbolTable.Name = "symbolTable";
+            this.symbolTable.ReadOnly = true;
+            this.symbolTable.Size = new System.Drawing.Size(300, 200);
+            this.symbolTable.TabIndex = 2;
+
+            // btn_Input
+            this.btn_Input.Location = new System.Drawing.Point(12, 120);
+            this.btn_Input.Name = "btn_Input";
+            this.btn_Input.Size = new System.Drawing.Size(75, 23);
+            this.btn_Input.TabIndex = 3;
+            this.btn_Input.Text = "Analyze";
+            this.btn_Input.UseVisualStyleBackColor = true;
+            this.btn_Input.Click += new System.EventHandler(this.btn_Input_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(640, 260);
+            this.Controls.Add(this.btn_Input);
+            this.Controls.Add(this.symbolTable);
+            this.Controls.Add(this.tfTokens);
+            this.Controls.Add(this.tfInput);
+            this.Name = "Form1";
+            this.Text = "Lexical Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ lexicalAnalyzerV2/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace lexicalAnalyzerV2
+{
+    public partial class Form1 : Form
+    {
+        // Declare necessary variables
+        List<List<string>> SymbolTable = new List<List<string>>(); // Symbol table to store variables
+        List<string> finalArray = new List<string>();
+        Regex variable_Reg;
+        Regex constants_Reg;
+        Regex operators_Reg;
+        Regex specialCharacters_Reg;
+        List<string> keywordList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize variables
+            keywordList = new List<string> { "int", "float", "while", "main", "if", "else", "new" };
+            variable_Reg = new Regex(@"^[A-Za-z_][A-Za-z0-9_]*$");
+            constants_Reg = new Regex(@"^[0-9]+(\.[0-9]+)?([eE][+-]?[0-9]+)?$");
+            operators_Reg = new Regex(@"[+\-*/=;(){}]");
+            specialCharacters_Reg = new Regex(@"[.,'\[\]{}();:?]");
+
+        }
+
+        // Button click event to start lexical analysis
+        private void btn_Input_Click(object sender, EventArgs e)
+        {
+            string userInput = tfInput.Text; // Get input from the text box
+            List<string> finalArrayc = new List<string>();
+            List<string> tempArray = new List<string>();
+
+            char[] charinput = userInput.ToCharArray(); // Convert input to char array
+
+            // Processing the input character by character
+            foreach (char ch in charinput)
+            {
+                Match matchVariable = variable_Reg.Match(ch.ToString());
+                Match matchConstant = constants_Reg.Match(ch.ToString());
+                Match matchOperator = operators_Reg.Match(ch.ToString());
+                Match matchSpecial = specialCharacters_Reg.Match(ch.ToString());
+
+                // If the character is a valid token, add it to tempArray
+                if (matchVariable.Success || matchConstant.Success || matchOperator.Success || matchSpecial.Success || ch == ' ')
+                {
+                    tempArray.Add(ch.ToString());
+                }
+
+                // If it's a newline or end of input, process tokens and reset tempArray
+                if (ch == '\n' || ch == ' ' || ch == '\r')
+                {
+                    if (tempArray.Count > 0)
+                    {
+                        finalArrayc.Add(string.Join("", tempArray));
+                        tempArray.Clear();
+                    }
+                }
+            }
+
+            // Process any remaining tokens after the loop
+            if (tempArray.Count > 0)
+            {
+                finalArrayc.Add(string.Join("", tempArray));
+                tempArray.Clear();
+            }
+
+            // Token processing and symbol table creation
+            tfTokens.Clear(); // Clear previous tokens
+            SymbolTable.Clear(); // Clear the symbol table
+
+            int row = 1; // Row index for symbol table
+            int count = 1; // Count for token identification
+
+            foreach (string token in finalArrayc)
+            {
+                Match matchOperator = operators_Reg.Match(token);
+                Match matchVariable = variable_Reg.Match(token);
+                Match matchConstant = constants_Reg.Match(token);
+                Match matchSpecial = specialCharacters_Reg.Match(token);
+
+                // Process operator tokens
+                if (matchOperator.Success)
+                {
+                    tfTokens.AppendText("<op, " + token + "> ");
+                }
+                // Process constant tokens
+                else if (matchConstant.Success)
+                {
+                    tfTokens.AppendText("<digit, " + token + "> ");
+                }
+                // Process special character tokens
+                else if (matchSpecial.Success)
+                {
+                    tfTokens.AppendText("<punc, " + token + "> ");
+                }
+                // Process variables (excluding keywords)
+                else if (matchVariable.Success && !keywordList.Contains(token))
+                {
+                    SymbolTable.Add(new List<string> { row.ToString(), token, "var", "", "line" + row });
+                    tfTokens.AppendText("<var" + count + ", " + token + "> ");
+                    row++;
+                    count++;
+                }
+                // Process keywords
+                else if (matchVariable.Success && keywordList.Contains(token))
+                {
+                    tfTokens.AppendText("<keyword, " + token + "> ");
+                }
+            }
+
+            // Display the symbol table
+            DisplaySymbolTable();
+        }
+
+        // Method to display the symbol table in the UI
+        private void DisplaySymbolTable()
+        {
+            symbolTable.Clear();
+            foreach (var row in SymbolTable)
+            {
+                symbolTable.AppendText(string.Join("\t", row) + "\n");
+            }
+        }
+    }
+}
